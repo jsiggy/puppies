12b35538dfcf14839273cc28edb946b3569ab0b9
john.sigler <john.sigler@schwab.com>
2018-04-09 14:03:50 -0500

    Add all pending steps

--- features/step_definitions/make_cheese_steps.rb
+++ features/step_definitions/make_cheese_steps.rb
@@ -1,3 +1,11 @@
 Given(/^I have no cheese$/) do
   pending
 end
+
+When(/^I press the make cheese button$/) do
+  pending
+end
+
+Then(/^I should have (\d+) piece of cheese$/) do |pieces|
+  pending
+end
