c0ddc1fd96ad31926c2175db263300396d2a9d26
john.sigler <john.sigler@schwab.com>
2018-04-09 19:30:07 -0500

    Extract magic numbers into constants and encapsulate private methods

--- lib/pages/shopping_cart_page.rb
+++ lib/pages/shopping_cart_page.rb
@@ -1,23 +1,29 @@
 class ShoppingCartPage
 
+  NAME_COLUMN = 1
+  SUBTOTAL_COLUMN = 3
+  LINES_PER_PUPPY = 6
+
   def initialize(browser)
     @browser = browser
   end
 
   def name_for_line_item(line_item)
-    cart_line_item(line_item)[1].text
+    cart_line_item(line_item)[NAME_COLUMN].text
   end
 
   def subtotal_for_line_item(line_item)
-    cart_line_item(line_item)[3].text
+    cart_line_item(line_item)[SUBTOTAL_COLUMN].text
   end
 
   def total
     @browser.td(class: 'total_cell').text
   end
 
+  private
+
   def row_for(line_item)
-    row = (line_item.to_i - 1) * 6
+    row = (line_item.to_i - 1) * LINES_PER_PUPPY
   end
 
   def cart_line_item(line_item)
